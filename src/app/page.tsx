ef67c2f787871ecf1b30006a408733e8b77f458e
Alex Newman <alex02new@outlook.com>
2025-06-29 21:42:32 -0400

    Added some basic routing and responsive navbar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/donate-now">Donate Now</a></li>
+        </ul>
+      </nav>
       <main className={styles.main}>
         <Image
           className={styles.logo}
